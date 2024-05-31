4ef783606aa67f1d46ed58a5168763987cbe2860
NuMeRo-999 <pedrovp003@gmail.com>
2024-05-31 22:27:06 +0200

    añadidas manos al heroe cuando no le faltan armas por equipar

--- src/components/Heroe.jsx
+++ src/components/Heroe.jsx
@@ -3,33 +3,48 @@ import { BASE_URL } from "../api/apiRequest";
 const Heroe = ({ heroe, weapons, isAttacking, stageId }) => {
   return (
     <div
-      className={`w-[256px] h-[173px] bg-no-repeat z-10 flex justify-center items-center  gap-14 pl-6 absolute left-[10%] top-[35%] ${
+      className={`w-[256px] h-[173px] bg-no-repeat z-10 flex justify-center items-center  gap-12 pl-6 absolute left-[10%] top-[35%] ${
         isAttacking && stageId === 1 ? "heroe-attack" : heroe?.state === 0 ? "heroe-die" : "heroe-idle"
       } ${isAttacking && stageId === 2 ? "heroe-hit" : ""}`}
       style={{
         backgroundImage: `url('${BASE_URL}/img/${heroe?.imageFilename}')`,
       }}
     >
-      {weapons.slice(0, 2).map((weapon, index) => (
-        <div key={index}>
-          <img
-            src={`${BASE_URL}/img/${weapon?.image}`}
-            alt=""
-            className={`item-animation ${
-              weapon?.image.includes("shield") || weapon?.image.includes("dagger")
-                ? "pt-16"
-                : "pb-12"
-            }`}
-          />
-        </div>
-      ))}
-      {weapons.length < 2 && (
+      {weapons.length === 0 ? (
+        <>
+          <div>
+            <img
+              src={`/src/assets/images/mano-der.png`}
+              alt=""
+              className="item-animation pt-24"
+            />
+          </div>
+          <div>
+            <img
+              src={`/src/assets/images/mano-izq.png`}
+              alt=""
+              className="item-animation pt-24"
+            />
+          </div>
+        </>
+      ) : (
+        weapons.slice(0, 2).map((weapon, index) => (
+          <div key={index}>
+            <img
+              src={`${BASE_URL}/img/${weapon?.image}`}
+              alt=""
+              className={`item-animation ${
+                weapon?.image.includes("shield") || weapon?.image.includes("dagger")
+                  ? "pt-16"
+                  : "pb-12"
+              }`}
+            />
+          </div>
+        ))
+      )}
+      {weapons.length == 1 && (
         <div>
-          <img
-            src=""
-            alt=""
-            className="item-animation w-12"
-          />
+          <img src={`/src/assets/images/mano-izq.png`} alt="" className="item-animation w-12 pt-24" />
         </div>
       )}
     </div>
