72f45e4830f568294b490c57edbf8f1079c86b3d
NuMeRo-999 <pedrovp003@gmail.com>
2024-06-02 00:14:07 +0200

    barra de vida

--- src/components/Enemies.jsx
+++ src/components/Enemies.jsx
@@ -12,12 +12,12 @@ const Enemies = ({ enemies, setSelectedEnemy, selectedEnemy, isAttacking, stageI
         }}
         onClick={() => setSelectedEnemy(enemies[0]?.state === 0 ? null : enemies[0]?.id)}
       >
-        {enemies[0]?.state !== 0 && (
-          <div className="bg-[url('/src/assets/images/level-health-bar.png')] bg-contain bg-bottom bg-no-repeat w-22 h-20">
-            <p className="font-m04 text-sm text-white pt-12 pl-[.70rem]">{enemies[0]?.level}</p>
-            <p className="font-m04 text-sm text-white pl-20 pt-5 pl-[.70rem]">{enemies[0]?.healthPoints}</p>
-          </div>
-        )}
+      {enemies[0]?.state !== 0 && (
+        <div className="bg-[url('/src/assets/images/level-health-bar.png')] bg-contain bg-bottom bg-no-repeat h-20">
+          <p className="font-m04 text-sm text-white pt-12 pl-[.70rem]">{enemies[0]?.level}</p>
+          <div className="bg-red-500 absolute max-w-32 top-[3.20rem] left-[3.3rem] h-2" style={{ width: `${enemies[0]?.healthPoints}%`, transition: 'width 0.5s' }}></div>
+        </div>
+      )}
       </div>
 
       <div
@@ -29,9 +29,9 @@ const Enemies = ({ enemies, setSelectedEnemy, selectedEnemy, isAttacking, stageI
         onClick={() => setSelectedEnemy(enemies[1]?.state === 0 ? null : enemies[1]?.id)}
       >
         {enemies[1]?.state !== 0 && (
-          <div className="bg-[url('/src/assets/images/level-health-bar.png')] bg-contain bg-bottom bg-no-repeat w-22 h-20">
+          <div className="bg-[url('/src/assets/images/level-health-bar.png')] bg-contain bg-bottom bg-no-repeat h-20">
             <p className="font-m04 text-sm text-white pt-12 pl-[.70rem]">{enemies[1]?.level}</p>
-            <p className="font-m04 text-sm text-white pt-5 pl-[.70rem]">{enemies[1]?.healthPoints}</p>
+            <div className="bg-red-500 absolute max-w-32 top-[3.20rem] left-[3.3rem] h-2" style={{ width: `${enemies[1]?.healthPoints}%`, transition: 'width 0.5s' }}></div>
           </div>
         )}
       </div>
@@ -45,9 +45,9 @@ const Enemies = ({ enemies, setSelectedEnemy, selectedEnemy, isAttacking, stageI
         onClick={() => setSelectedEnemy(enemies[2]?.state === 0 ? null : enemies[2]?.id)}
       >
         {enemies[2]?.state !== 0 && (
-          <div className="bg-[url('/src/assets/images/level-health-bar.png')] bg-contain bg-bottom bg-no-repeat w-22 h-20">
+          <div className="bg-[url('/src/assets/images/level-health-bar.png')] bg-contain bg-bottom bg-no-repeat h-20">
             <p className="font-m04 text-sm text-white pt-12 pl-[.70rem]">{enemies[2]?.level}</p>
-            <p className="font-m04 text-sm text-white pt-5 pl-[.70rem]">{enemies[2]?.healthPoints}</p>
+            <div className="bg-red-500 absolute max-w-32 top-[3.20rem] left-[3.3rem] h-2" style={{ width: `${enemies[2]?.healthPoints}%`, transition: 'width 0.5s' }}></div>
           </div>
         )}
       </div>
