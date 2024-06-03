f5449ae07d541a04bf2fa7df124b505c2c0a9848
NuMeRo-999 <pedrovp003@gmail.com>
2024-06-03 22:14:23 +0200

    Arreglada barra de vida enemigos

--- src/components/Enemies.jsx
+++ src/components/Enemies.jsx
@@ -2,11 +2,12 @@ import { BASE_URL } from "../api/apiRequest";
 import PlayAudio from "../utils/PlayAudio";
 
 const Enemies = ({ enemies, setSelectedEnemy, selectedEnemy, isAttacking, stageId }) => {
+  const maxHealthWidth = 67;
 
   return (
     <>
       <div
-        className={`size-[192px] bg-no-repeat z-10 absolute ${selectedEnemy === enemies[0]?.id ? 'drop-shadow-[0_50px_50px_rgba(255,255,255,1)]' : '' } hover:drop-shadow-[0_50px_50px_rgba(255,255,255,1)] image-cursor right-[10%] top-[20%] ${enemies[0]?.state === 0 ? 'enemy-die bg-left-bottom' : 'enemy-idle bg-right-top'} ${isAttacking && enemies[0]?.id === selectedEnemy ? 'enemy-hit' : ''} ${isAttacking && enemies[0]?.state !== 0 && stageId === 2 ? 'enemy-attack-2' : ''}`}
+        className={`size-[192px] bg-no-repeat z-10 absolute ${selectedEnemy === enemies[0]?.id ? 'drop-shadow-[0_50px_50px_rgba(255,255,255,1)]' : ''} hover:drop-shadow-[0_50px_50px_rgba(255,255,255,1)] image-cursor right-[10%] top-[20%] ${enemies[0]?.state === 0 ? 'enemy-die bg-left-bottom' : 'enemy-idle bg-right-top'} ${isAttacking && enemies[0]?.id === selectedEnemy ? 'enemy-hit' : ''} ${isAttacking && enemies[0]?.state !== 0 && stageId === 2 ? 'enemy-attack-2' : ''}`}
         id={enemies[0]?.id}
         style={{
           backgroundImage: `url('${BASE_URL}/img/${enemies[0]?.imageFilename}')`,
@@ -16,16 +17,16 @@ const Enemies = ({ enemies, setSelectedEnemy, selectedEnemy, isAttacking, stageI
           PlayAudio("/src/assets/sounds/Cursor2.ogg");
         }}
       >
-      {enemies[0]?.state !== 0 && (
-        <div className="bg-[url('/src/assets/images/level-health-bar.png')] bg-contain bg-bottom bg-no-repeat h-20">
-          <p className="font-m04 text-sm text-white pt-[2.7rem] pl-[.8rem]">{enemies[0]?.level < 10 ? `0${enemies[0]?.level}` : enemies[0]?.level}</p>
-          <div className="bg-red-500 absolute max-w-32 top-[3.20rem] left-[3.3rem] h-2" style={{ width: `${enemies[0]?.healthPoints}%`, transition: 'width 0.5s' }}></div>
-        </div>
-      )}
+        {enemies[0]?.state !== 0 && (
+          <div className="bg-[url('/src/assets/images/level-health-bar.png')] bg-contain bg-bottom bg-no-repeat h-20">
+            <p className="font-m04 text-sm text-white pt-[2.7rem] pl-[.8rem]">{enemies[0]?.level < 10 ? `0${enemies[0]?.level}` : enemies[0]?.level}</p>
+            <div className="bg-red-500 absolute top-[3.20rem] left-[3.3rem] h-2" style={{ width: `${(enemies[0]?.healthPoints / enemies[0]?.healthPoints) * maxHealthWidth}%`, transition: 'width 0.5s' }}></div>
+          </div>
+        )}
       </div>
 
       <div
-        className={`size-[192px] bg-no-repeat z-10 absolute ${selectedEnemy === enemies[1]?.id ? 'drop-shadow-[0_50px_50px_rgba(255,255,255,1)]' : '' } hover:drop-shadow-[0_50px_50px_rgba(255,255,255,1)] image-cursor right-[20%] top-[30%] ${enemies[1]?.state === 0 ? 'enemy-die bg-left-bottom' : 'enemy-idle bg-right-top'} ${isAttacking && enemies[1]?.id === selectedEnemy ? 'enemy-hit' : ''} ${isAttacking && enemies[1]?.state !== 0 && stageId === 2 ? 'enemy-attack' : ''}`}
+        className={`size-[192px] bg-no-repeat z-10 absolute ${selectedEnemy === enemies[1]?.id ? 'drop-shadow-[0_50px_50px_rgba(255,255,255,1)]' : ''} hover:drop-shadow-[0_50px_50px_rgba(255,255,255,1)] image-cursor right-[20%] top-[30%] ${enemies[1]?.state === 0 ? 'enemy-die bg-left-bottom' : 'enemy-idle bg-right-top'} ${isAttacking && enemies[1]?.id === selectedEnemy ? 'enemy-hit' : ''} ${isAttacking && enemies[1]?.state !== 0 && stageId === 2 ? 'enemy-attack' : ''}`}
         id={enemies[1]?.id}
         style={{
           backgroundImage: `url('${BASE_URL}/img/${enemies[1]?.imageFilename}')`,
@@ -38,13 +39,13 @@ const Enemies = ({ enemies, setSelectedEnemy, selectedEnemy, isAttacking, stageI
         {enemies[1]?.state !== 0 && (
           <div className="bg-[url('/src/assets/images/level-health-bar.png')] bg-contain bg-bottom bg-no-repeat h-20">
             <p className="font-m04 text-sm text-white pt-[2.7rem] pl-[.8rem]">{enemies[1]?.level < 10 ? `0${enemies[1]?.level}` : enemies[1]?.level}</p>
-            <div className="bg-red-500 absolute max-w-32 top-[3.20rem] left-[3.3rem] h-2" style={{ width: `${enemies[1]?.healthPoints}%`, transition: 'width 0.5s' }}></div>
+            <div className="bg-red-500 absolute top-[3.20rem] left-[3.3rem] h-2" style={{ width: `${(enemies[1]?.healthPoints / enemies[1]?.healthPoints) * maxHealthWidth}%`, transition: 'width 0.5s' }}></div>
           </div>
         )}
       </div>
-      
+
       <div
-        className={`size-[192px] bg-no-repeat z-10 absolute ${selectedEnemy === enemies[2]?.id ? 'drop-shadow-[0_50px_50px_rgba(255,255,255,1)]' : '' } hover:drop-shadow-[0_50px_50px_rgba(255,255,255,1)] image-cursor right-[10%] top-[40%] ${enemies[2]?.state === 0 ? 'enemy-die bg-left-bottom' : 'enemy-idle bg-right-top'} ${isAttacking && enemies[2]?.id === selectedEnemy ? 'enemy-hit' : ''} ${isAttacking && enemies[2]?.state !== 0 && stageId === 2 ? 'enemy-attack-3' : ''}`}
+        className={`size-[192px] bg-no-repeat z-10 absolute ${selectedEnemy === enemies[2]?.id ? 'drop-shadow-[0_50px_50px_rgba(255,255,255,1)]' : ''} hover:drop-shadow-[0_50px_50px_rgba(255,255,255,1)] image-cursor right-[10%] top-[40%] ${enemies[2]?.state === 0 ? 'enemy-die bg-left-bottom' : 'enemy-idle bg-right-top'} ${isAttacking && enemies[2]?.id === selectedEnemy ? 'enemy-hit' : ''} ${isAttacking && enemies[2]?.state !== 0 && stageId === 2 ? 'enemy-attack-3' : ''}`}
         id={enemies[2]?.id}
         style={{
           backgroundImage: `url('${BASE_URL}/img/${enemies[2]?.imageFilename}')`,
@@ -57,7 +58,7 @@ const Enemies = ({ enemies, setSelectedEnemy, selectedEnemy, isAttacking, stageI
         {enemies[2]?.state !== 0 && (
           <div className="bg-[url('/src/assets/images/level-health-bar.png')] bg-contain bg-bottom bg-no-repeat h-20">
             <p className="font-m04 text-sm text-white pt-[2.7rem] pl-[.8rem]">{enemies[2]?.level < 10 ? `0${enemies[2]?.level}` : enemies[2]?.level}</p>
-            <div className="bg-red-500 absolute max-w-32 top-[3.20rem] left-[3.3rem] h-2" style={{ width: `${enemies[2]?.healthPoints}%`, transition: 'width 0.5s' }}></div>
+            <div className="bg-red-500 absolute top-[3.20rem] left-[3.3rem] h-2" style={{ width: `${(enemies[2]?.healthPoints / enemies[2]?.healthPoints) * maxHealthWidth}%`, transition: 'width 0.5s' }}></div>
           </div>
         )}
       </div>
